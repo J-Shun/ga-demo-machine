9899c4c58e2b029af319a9dcf37473b1c28f78fa
Shun_Tseng <shun_tseng@migocorp.com>
2023-12-28 10:52:28 +0800

    init

---
+++ app.js
@@ -0,0 +1,23 @@
+const box1 = document.querySelector('.box1');
+const box2 = document.querySelector('.box2');
+const box3 = document.querySelector('.box3');
+const box4 = document.querySelector('.box4');
+
+const toggle = document.querySelector('.toggle');
+
+let isToggle = false;
+
+toggle.addEventListener('click', () => {
+  if (!isToggle) {
+    box1.style.backgroundColor = '#fa0';
+    box2.style.backgroundColor = '#f3a';
+    box3.style.backgroundColor = '#a6f';
+    box4.style.backgroundColor = '#66ff8c';
+  } else {
+    box1.style.backgroundColor = '#86c6fd';
+    box2.style.backgroundColor = '#86c6fd';
+    box3.style.backgroundColor = '#86c6fd';
+    box4.style.backgroundColor = '#86c6fd';
+  }
+  isToggle = !isToggle;
+});
